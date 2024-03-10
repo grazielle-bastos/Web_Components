b1a0ff261e76886e3bdeb7e7462eaa18455e3796
Grazielle Bastos <grazielle1101@gmail.com>
2024-03-10 18:06:52 -0300

    Finalização Web Components

--- src/components/CardNews.js
+++ src/components/CardNews.js
@@ -2,8 +2,88 @@ class CardNews extends HTMLElement {
     constructor() {
         super();
 
-        const shadow = this.attachShadow({mode:"open"});
-        shadow.innerHTML = "<h1>Hello World</h1>";
+        const shadow = this.attachShadow({mode: "open"});
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.styles());
+    }
+
+    build() {
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card");
+
+        const cardLeft = document.createElement("div");
+        cardLeft.setAttribute("class", "card__left");
+
+        const autor = document.createElement("span");
+        autor.textContent = "By " + (this.getAttribute("autor") || "Anonymous");
+
+        const linkTitle = document.createElement("a");
+        linkTitle.textContent = this.getAttribute("title");
+        linkTitle.href = this.getAttribute("link-url")
+
+        const newsContent = document.createElement("p");
+        newsContent.textContent = this.getAttribute("content");
+
+        cardLeft.appendChild(autor);
+        cardLeft.appendChild(linkTitle);
+        cardLeft.appendChild(newsContent);
+       
+
+        const cardRight = document.createElement("div");
+        cardRight.setAttribute("class", "card__right");
+
+        const newsImage = document.createElement("img");
+        newsImage.src = this.getAttribute("photo") || "assets/foto-default.jpg";
+        newsImage.alt = "Homem em frente ao setup programando";
+        cardRight.appendChild(newsImage);
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+
+        return componentRoot;
+    }
+
+    styles() {
+        const style = document.createElement("style")
+        style.textContent = `
+        .card{
+            width: 80%;
+            border: 1px solid gray;
+            display:flex;
+            flex-direction: row;
+            -webkit-box-shadow: 6px 7px 15px -2px rgba(0,0,0,0.55);
+            -moz-box-shadow: 6px 7px 15px -2px rgba(0,0,0,0.55);
+            box-shadow: 6px 7px 15px -2px rgba(0,0,0,0.55);
+            justify-content: space-between;
+        }
+        
+        .card__left {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            padding-left: 10px;
+        }
+        
+        .card__left > span {
+            font-weight: 400;
+        }
+        
+        .card__left > a {
+            margin-top: 15px;
+            font-size: 25px;
+            color: black;
+            text-decoration: none;
+            font-weight: bold;
+        }
+        
+        
+        .card__left > p {
+            color: gray;
+        }
+
+    `;
+
+        return style;
     }
 }
 
